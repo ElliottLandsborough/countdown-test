32212a7382f215ea329c2f3049bb98f5e2cb2049
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-06-04 09:56:01 +0100

    Properly separate date class

--- src/Controller/CountdownController.php
+++ src/Controller/CountdownController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use DateTime;
 use App\Utils\DateClass;
 
 class CountdownController extends AbstractController
@@ -12,77 +11,26 @@ class CountdownController extends AbstractController
 
     public function __construct(DateClass $dateClass)
     {
-        $this->dateClass = $dateClass;
+        $this->dateClass = $dateClass->setBirthday('1987-07-10');
     }
 
     public function countdown()
     {
-        $today = date('Y-m-d');
-
-        // birthday - in 'yyyy-mm-dd' format
-        $birthday = '1987-07-10';
-
-        // my next birthday string
-        $nextBirthDayString = date('Y') . '-' . substr($birthday, 5, 5);
-
-        // convert next birthday into time
-        $nextBirthdayTime = strtotime($nextBirthDayString);
-
-        // if birthday has happend this year (or is today), skip to next year
-        if ($today >= $nextBirthDayString) {
-            // make a new date based on next year
-            $nextBirthDayString = date('Y-' . substr($birthday, 5, 5) . '', strtotime($nextBirthDayString . ' +1 year'));
-        }
-
-        // convert next birthday into date
-        $nextBirthDayDate = date($nextBirthDayString);
-
-        // create a next birthday object
-        $nextBirthdayObject = new DateTime($nextBirthDayString);
-
-        // format my next birthday in a readable way
-        $nextBirthdayStringFormatted = $nextBirthdayObject->format('l jS F Y');
-
-        // my birthday string
-        $birthdayString = $birthday;
-
-        // create a birthday object
-        $birthdayObject = new DateTime($birthdayString);
-
-        // format my birthday in a readable way
-        $birthdayStringFormatted = $birthdayObject->format('l jS F Y');
-
-        // compare birthday with today/now
-        $birthdayDiff = $birthdayObject->diff(new DateTime('now'));
-
-        // my current age
-        $currentAge = $birthdayDiff->y;
-
-        // compare NEXT birthday with today/now
-        $birthdayDiff = $nextBirthdayObject->diff(new DateTime('now'));
-
-        // get time until next birthday
-        $nextBirthdayMonths = $birthdayDiff->m;
-        $nextBirthdayDays = $birthdayDiff->d;
-        $nextBirthdayHours = $birthdayDiff->h;
-        $nextBirthdayMinutes = $birthdayDiff->i;
-        $nextBirthdaySeconds = $birthdayDiff->s;
-
-        // my next age
-        $nextAge = intval($currentAge) + 1;
+        $birthdayDiff = $this->dateClass->getNextBirthdayDiff();
+        $currentAge = $this->dateClass->getCurrentage();
 
         return $this->render(
             'default/countdown.html.twig',
             [
-                'birthdayStringFormatted' => $birthdayStringFormatted,
-                'nextBirthdayStringFormatted' => $nextBirthdayStringFormatted,
+                'nextBirthdayStringFormatted' => $this->dateClass->getReadableNextBirthdayString(),
+                'birthdayStringFormatted' => $this->dateClass->getReadableBirthdayString(),
                 'currentAge' => $currentAge,
-                'nextAgePretty' => $this->dateClass->ordinal($nextAge),
-                'nextBirthdayMonths' => $nextBirthdayMonths,
-                'nextBirthdayDays' => $nextBirthdayDays,
-                'nextBirthdayHours' => $nextBirthdayHours,
-                'nextBirthdayMinutes' => $nextBirthdayMinutes,
-                'nextBirthdaySeconds' => $nextBirthdaySeconds,
+                'nextAgePretty' => $this->dateClass->ordinal($currentAge + 1),
+                'nextBirthdayMonths' => $birthdayDiff->m,
+                'nextBirthdayDays' => $birthdayDiff->d,
+                'nextBirthdayHours' => $birthdayDiff->h,
+                'nextBirthdayMinutes' => $birthdayDiff->i,
+                'nextBirthdaySeconds' => $birthdayDiff->s,
             ]
         );
     }
--- src/Utils/DateClass.php
+++ src/Utils/DateClass.php
@@ -2,8 +2,127 @@
 // src/Utils/DateClass.php
 namespace App\Utils;
 
+use DateTime;
+
 class DateClass
 {
+    protected $today;
+    protected $birthdayString; // birthday - in 'yyyy-mm-dd' format
+    protected $birthdayObject;
+    protected $nextBirthdayString;
+    protected $nextBirthdayObject;
+    protected $currentAge;
+
+    public function __construct()
+    {
+        $this->today = date('Y-m-d');
+    }
+
+    public function setBirthday(string $string)
+    {
+        $this->setBirthdayString($string);
+        $this->setBirthdayObject();
+        $this->setNextBirthdayString();
+        $this->setNextBirthdayObject();
+        $this->setCurrentAge();
+
+        return $this;
+    }
+
+    public function setBirthdayString(string $string)
+    {
+        $this->birthdayString = $string;
+
+        return $this;
+    }
+
+    public function getBirthdayString()
+    {
+        return $this->birthdayString;
+    }
+
+    public function getBirthdayObject()
+    {
+        return $this->birthdayObject;
+    }
+
+    public function setBirthdayObject()
+    {
+        $this->birthdayObject = new DateTime($this->getBirthdayString());
+
+        return $this;
+    }
+
+    public function getNextBirthdayString()
+    {
+        return $this->nextBirthdayString;
+    }
+
+    public function setNextBirthdayString()
+    {
+        // my next birthday string
+        $nextBirthdayString = date('Y') . '-' . substr($this->birthdayString, 5, 5);
+
+        // convert next birthday into time
+        $nextBirthdayTime = strtotime($nextBirthdayString);
+
+        // if birthday has happend this year (or is today), skip to next year
+        if ($this->today >= $nextBirthdayString) {
+            // make a new date based on next year
+            $nextBirthdayString = date('Y-' . substr($this->birthdayString, 5, 5) . '', strtotime($nextBirthdayString . ' +1 year'));
+        }
+
+        $this->nextBirthdayString = $nextBirthdayString;
+
+        return $this;
+    }
+
+    public function getNextBirthdayObject()
+    {
+        return $this->nextBirthdayObject;
+    }
+
+    public function setNextBirthdayObject()
+    {
+        $this->nextBirthdayObject = new DateTime($this->getNextBirthdayString());
+
+        return $this;
+    }
+
+    public function getReadableNextBirthdayString()
+    {
+        return $this->getNextBirthdayObject()->format('l jS F Y');
+    }
+
+    public function getReadableBirthdayString()
+    {
+        return $this->getBirthdayObject()->format('l jS F Y');
+    }
+
+    // compare birthday with today/now
+    public function getBirthdayDiff()
+    {
+        return $this->getBirthdayObject()->diff(new DateTime('now'));
+    }
+
+    public function getNextBirthdayDiff()
+    {
+        return $this->getNextBirthdayObject()->diff(new DateTime('now'));
+    }
+
+    public function setCurrentAge()
+    {
+        // my current age
+        $this->currentAge = $this->getBirthdayDiff()->y;
+
+        return $this;
+    }
+
+    public function getCurrentage()
+    {
+        return $this->currentAge;
+    }
+
     public function ordinal(int $number)
     {
         $ends = array('th','st','nd','rd','th','th','th','th','th','th');
